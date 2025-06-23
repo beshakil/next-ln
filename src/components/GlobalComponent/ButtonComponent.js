7a9da01f708c2751cac2fc51cb749e02bda96d74
Shakil Ahmed <dev.shakilshajib@gmail.com>
2025-06-23 23:45:41 +0600

    task design

--- src/components/GlobalComponent/ButtonComponent.js
+++ src/components/GlobalComponent/ButtonComponent.js
@@ -4,7 +4,7 @@ const ButtonComponent = ({ onClick, buttonClass, buttonText, startContent }) => 
     return (
         <button
             onClick={onClick}
-            className={`${buttonClass} flex items-center justify-center h-10 gap-1.5 bg-purple-600 hover:bg-purple-500 text-sm text-zinc-50 px-3 rounded-lg`} color="primary" type="submit">
+            className={`${buttonClass} flex items-center cursor-pointer justify-center h-10 gap-1.5 bg-purple-600 hover:bg-purple-500 text-sm text-zinc-50 px-3 rounded-lg`} color="primary" type="submit">
             {startContent}{buttonText}
         </button>
     );
